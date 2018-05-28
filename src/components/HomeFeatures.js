054db6fab4e27248e2f2919fdbe19fbf96e0fdfe
Sergey Khmelevskoy <xs290@me.com>
2018-05-28 17:10:24 +0300

    home features section

--- src/components/HomeFeatures.js
+++ src/components/HomeFeatures.js
@@ -9,9 +9,55 @@ export default class HomeFeatures extends Component {
             <span classname="t-small">How are we different</span>
             <h2>We are built for the modern business</h2>
           </div>
+        </div>
+        <div className="home-features__bg-holder">
+          <div className="container container--narrow">
+            <div className="home-features__wrapper">
+              <div className="home-features__col">
+                <div className="home-features-card">
+                  <div className="home-features-card__image">
 
-          <div className="home-features__wrapper">
+                  </div>
+                  <div className="home-features-card__content">
+                    <div className="home-features-card__name">Fully digital</div>
+                    <p>Powered by real humans. We spend time to understand your business and what is most important to you. Our reports are tailored to the needs of your business.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="home-features__col">
+                <div className="home-features-card">
+                  <div className="home-features-card__image">
 
+                  </div>
+                  <div className="home-features-card__content">
+                    <div className="home-features-card__name">Powered by thoughtful humans</div>
+                    <p>Powered by real humans. We spend time to understand your business and what is most important to you. Our reports are tailored to the needs of your business.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="home-features__col">
+                <div className="home-features-card">
+                  <div className="home-features-card__image">
+
+                  </div>
+                  <div className="home-features-card__content">
+                    <div className="home-features-card__name">Tailored for your industry</div>
+                    <p>Powered by real humans. We spend time to understand your business and what is most important to you. Our reports are tailored to the needs of your business.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="home-features__col">
+                <div className="home-features-card">
+                  <div className="home-features-card__image">
+
+                  </div>
+                  <div className="home-features-card__content">
+                    <div className="home-features-card__name">Less costly than hiring internally</div>
+                    <p>Powered by real humans. We spend time to understand your business and what is most important to you. Our reports are tailored to the needs of your business.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
