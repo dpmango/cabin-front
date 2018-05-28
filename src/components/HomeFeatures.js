2b947f25a3cd62f3aae0c8666459b30bc8a942e7
Sergey Khmelevskoy <xs290@me.com>
2018-05-28 16:00:07 +0300

    home to components

---
+++ src/components/HomeFeatures.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class HomeFeatures extends Component {
+  render(){
+    return(
+      <div className="home-features">
+        <div className="container container--narrow">
+          <div className="t-center">
+            <span classname="t-small">How are we different</span>
+            <h2>We are built for the modern business</h2>
+          </div>
+
+          <div className="home-features__wrapper">
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,10 +6,12 @@ import PropTypes from 'prop-types';
 import { SET_HEADER_CLASS } from '../store/ActionTypes';
 import api from '../services/Api';
 
-import HomeLogosEl from '../components/HomeLogosEl';
 import GetStartedBottom from '../components/GetStartedBottom';
-import SignupEmail from '../components/SignupEmail';
+import HomeHero from '../components/HomeHero';
+import HomeLogos from '../components/HomeLogos';
+import HomePromo from '../components/HomePromo';
 import HomeBenefits from '../components/HomeBenefits';
+import HomeFeatures from '../components/HomeFeatures';
 
 class Home extends Component {
   static propTypes = {
@@ -31,50 +33,11 @@ class Home extends Component {
           <title>Homepage || CABIN</title>
         </Helmet>
 
-        <div className="home-hero">
-          <div className="container">
-            <div className="home-hero__wrapper">
-              <h1>Turn messy documents into accurate financial statements.</h1>
-              <div className="home-hero__desc">Online accounting and corporate secretary service starting at <span>$200 per month</span></div>
-              <SignupEmail extraClass="home-hero__form" />
-            </div>
-          </div>
-        </div>
-
-        <div className="home-logos">
-          <div className="container">
-            <div className="home-logos__wrapper">
-              <span className="home-logos__name">Trusted by:</span>
-              <div className="home-logos__list">
-                <HomeLogosEl iconName="grain" tooltipContent="Grain tooltip content" />
-                <HomeLogosEl iconName="bbounce" tooltipContent="bbounce tooltip content" />
-                <HomeLogosEl iconName="canny" tooltipContent="canny tooltip content" />
-                <HomeLogosEl iconName="elements" tooltipContent="Wellness and spa" />
-                <HomeLogosEl iconName="seventeen" tooltipContent="seventeen tooltip content" />
-                <HomeLogosEl iconName="poh" tooltipContent="Poh tooltip content" />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="home-promo">
-          <div className="container container--narrow">
-            <div className="home-promo__wrapper">
-              <div className="home-promo__left">
-                <div className="home-promo__image">
-                  <img src={require('../images/homePromoList.png')} srcSet={require('../images/homePromoList@2x.png')  + ' 2x'} alt=""/>
-                </div>
-              </div>
-              <div className="home-promo__right">
-                <div className="t-small">Why we built Cabin</div>
-                <h2>You focus on your business, we take care of everything else.</h2>
-                <p className="t-paragraph">Growing a business is tough. You have lots to do. Do not be distracted by bookkeeping, accounting, compliance, taxes, payroll, and other chores. Let us take that off your hands.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <HomeHero />
+        <HomeLogos />
+        <HomePromo />
         <HomeBenefits />
+        <HomeFeatures />
 
         <GetStartedBottom />
 
