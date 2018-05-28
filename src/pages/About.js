091330217441458968fdaeb84b9273c839b4fda9
Sergey Khmelevskoy <xs290@me.com>
2018-05-28 23:00:40 +0300

    about updated, added swiper

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,6 +7,7 @@ import { SET_HEADER_CLASS } from '../store/ActionTypes';
 import api from '../services/Api';
 
 import GetStartedBottom from '../components/GetStartedBottom';
+import Professionals from '../components/Professionals';
 
 class About extends Component {
   static propTypes = {
@@ -25,14 +26,62 @@ class About extends Component {
     return (
       <div className="text-page">
         <div className="hero">
-          <div className="container">
+          <div className="hero__bg">
+            <img src={require('../images/aboutHeroImg.png')} srcSet={require('../images/aboutHeroImg@2x.png')  + ' 2x'} alt=""/>
+          </div>
+          <div className="container container--narrow">
             <div className="hero__wrapper">
               <span className="t-small">About us</span>
               <h2>Powered by thoughtful humans</h2>
+              <p className="t-paragraph">Founded by an experienced team that understands your business</p>
             </div>
-
           </div>
         </div>
+
+        <div className="title-section">
+          <div className="container">
+            <div className="title-section__wrapper">
+              <h2>Our Mission</h2>
+              <p className="t-paragraph">We make office operations simple and fuss-free for small and medium businesses. We bring clarity to the numbers and help business owners make better decisions. </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="people">
+          <div className="people__wrapper">
+            <div className="people__left">
+              <div className="people__image">
+                <img src={require('../images/aboutPeople.jpg')} srcSet={require('../images/aboutPeople@2x.jpg')  + ' 2x'} alt=""/>
+              </div>
+            </div>
+            <div className="people__right">
+              <div className="people__content">
+                <span className="t-small">People</span>
+                <h2>Cabin is founded by an experienced team that understands your business.</h2>
+                <div className="people__person">
+                  <div className="people__person-title"><span>Rifeng Gao,</span> Co-Founder</div>
+                  <div className="people__person-description">
+                    CFO and Co-Founder, Grain<br/>
+                    Associate Consultant, Bain & Company <br/>
+                    BSc (Hons), London School of Economics <br/>
+                    Forbes 30 under 30
+                  </div>
+                </div>
+                <div className="people__person">
+                  <div className="people__person-title"><span>Isaac Loh,</span> Co-Founder</div>
+                  <div className="people__person-description">
+                    Senior Business Analyst, Vance Group<br/>
+                    MSc. Cambridge University, Fitzwilliam<br/>
+                    College Honorary Cambridge <br/>
+                    Commonwealth Trust Scholar
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Professionals />
 
         <GetStartedBottom />
 
