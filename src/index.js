96053f3a93391780fa0e0770faa471d187f5160a
Sergey Khmelevskoy <xs290@me.com>
2018-07-17 01:15:38 +0300

    add react-snap

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import App from './App';
 import './css/app.css';
 // import registerServiceWorker from './registerServiceWorker';
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootElement
+  );
+} else {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootElement
+  );
+}
+
+
 
 // registerServiceWorker();
